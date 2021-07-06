6a840fda89643986193b8428e134cb34f9b8054c
Yadir Rojas Delgado <yadirgerardo@gmail.com>
2021-07-06 13:08:32 -0600

    made 3d component almost ready to be used

---
+++ src/Components/Model.js
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+extend({ OrbitControls });
+
+function Model() {
+  const Controls = () => {
+    const controls = useRef();
+
+    const { camera, gl } = useThree();
+
+    useFrame(() => {
+      controls.current.update();
+    });
+
+    return (
+      <orbitControls
+        ref={controls}
+        autoRotate
+        args={[camera, gl.domElement]}
+      ></orbitControls>
+    );
+  };
+
+  return (
+    <div>
+      <Canvas>
+        <mesh>
+          <Controls />
+          <boxBufferGeometry
+            attach="geometry"
+            args={[4, 4, 4]}
+          ></boxBufferGeometry>
+          <meshBasicMaterial
+            wireframe
+            attach="material"
+            color="white"
+          ></meshBasicMaterial>
+        </mesh>
+      </Canvas>
+    </div>
+  );
+}
+
+export default Model;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import AutoScrollGallery from './AutoScrollGallery';
-import NavBar from './NavBar';
-import SlidingInfo from './SlidingInfo';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import AutoScrollGallery from "./AutoScrollGallery";
+import NavBar from "./NavBar";
+import SlidingInfo from "./SlidingInfo";
+import Component from "./Components/Component";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <NavBar />
     <AutoScrollGallery />
     <SlidingInfo />
+    <Component />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
