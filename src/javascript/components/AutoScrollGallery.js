d4e5e5881c9f487e1a7cf4c427ad89d0cbfa9b89
Yadir Rojas Delgado <yadirgerardo@gmail.com>
2021-07-14 21:26:45 -0600

    fixed minor issue with autoscroll gallery

--- src/javascript/components/AutoScrollGallery.js
+++ src/javascript/components/AutoScrollGallery.js
@@ -17,14 +17,15 @@ window.onload = () => {
   const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
     "--marquee-elements-displayed"
   );
-  const marqueeContent = document.querySelector("ul.marquee-content");
+  console.log(marqueeElementsDisplayed);
+  const marqueeContent = document.querySelector(".marquee-content");
   if (marqueeContent != null) {
     root.style.setProperty(
       "--marquee-elements",
       marqueeContent.children.length
     );
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < marqueeElementsDisplayed; i++) {
       marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
     }
 
